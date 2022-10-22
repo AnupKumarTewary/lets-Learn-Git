2a0dd69069ad97e9d9cdb8686fb9dd8720fb0cc6
Anup kumar Tewary <anuptewary549@gmail.com>
2022-10-22 12:25:46 +0530

    added multiply.jav

---
+++ multiply.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main(){
+    
+    int a = 6;
+    int b = 5;
+
+    cout<<"The product is "<<a *b<<endl;
+    return 0;
+}
